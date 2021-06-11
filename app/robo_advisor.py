ca2e4c6a5252a21fb63920bcadf88180673c5915
jts581 <jts581@stern.nyu.edu>
2021-06-10 20:15:23 -0400

    Request functionality enabled

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -1,1 +1,21 @@
-print("TODO: make a web request for some stock data")
+
+import os
+from dotenv import load_dotenv
+import requests
+
+load_dotenv() # get env vars from the .env file
+
+# read env variables
+
+APHAVANTAGE_API_KEY = os.getenv("ALPHAVANTAGE_API_KEY")
+
+# make a request
+
+symbol = "MSFT" # ask for user input
+
+
+request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol={symbol}&apikey={APHAVANTAGE_API_KEY}"
+
+response = requests.get(request_url)
+print(type(response))
+print(response.text)
